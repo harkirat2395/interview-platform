5f034517f94437acb1481eb6e2d47f3e4037d9d7
harkirat2395 <harkirat2395@gmail.com>
2025-11-20 14:51:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
+# Install system dependencies for Azure
 apt-get update
-apt-get install -y libgl1-mesa-glx libglib2.0-0 libsm6 libxrender1 libxext6 libasound2 libatk-bridge2.0-0 libnss3
+apt-get install -y \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    libsm6 \
+    libxrender1 \
+    libxext6 \
+    libasound2 \
+    libatk-bridge2.0-0 \
+    libnss3 \
+    libgstreamer1.0-0 \
+    gstreamer1.0-plugins-base \
+    gstreamer1.0-plugins-good \
+    gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-ugly \
+    gstreamer1.0-libav \
+    gstreamer1.0-doc \
+    gstreamer1.0-tools \
+    gstreamer1.0-x \
+    gstreamer1.0-alsa \
+    gstreamer1.0-pulseaudio
+
+# Create necessary directories
+mkdir -p ~/.cache
+mkdir -p ~/.config
